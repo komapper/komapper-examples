66a163007ec27fbea6886f2fe2903c2a10119a75
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-15 16:54:15 +0900

    Update Komapper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 pluginManagement {
+    val komapperVersion: String by settings
     val kspVersion: String by settings
     repositories {
         gradlePluginPortal()
         google()
     }
     plugins {
+        id("org.komapper.gradle") version komapperVersion
         id("com.google.devtools.ksp") version kspVersion
     }
 }
