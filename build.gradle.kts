48e37837bc0cc8436e94e9f4e70676a6df96be16
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-14 14:42:15 +0900

    Refactor (#53)
    * Change the task name
    
    * Use gradle-build-action
    
    * Set timeout-minutes
    
    * Polish build.gradle.kts
    
    * Update ktlint version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
+    java
     kotlin("jvm")
     id("com.diffplug.spotless") version "6.0.0"
 }
 
-val ktlintVersion = "0.41.0"
+val ktlintVersion: String by project
 
 val springBootProjects = subprojects.filter {
     it.name.startsWith("spring-boot")
@@ -12,7 +13,11 @@ val springBootProjects = subprojects.filter {
 allprojects {
     apply(plugin = "com.diffplug.spotless")
 
-    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    repositories {
+        mavenCentral()
+    }
+
+    spotless {
         kotlinGradle {
             ktlint(ktlintVersion)
         }
@@ -23,32 +28,28 @@ allprojects {
             dependsOn(spotlessApply)
         }
     }
-
-    repositories {
-        mavenCentral()
-    }
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-
     dependencies {
         if (project !in springBootProjects) {
-            "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.8.1")
-            "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+            testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+            testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
         }
     }
 
     if (project.name != "codegen") {
-        configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        spotless {
             kotlin {
                 targetExclude("build/**")
                 ktlint(ktlintVersion)
             }
         }
+    }
+
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
     }
 }
