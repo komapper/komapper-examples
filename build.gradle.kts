da792e407223fae82b86b7b3f32c26414ead386c
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-21 15:28:57 +0900

    Add an example to compare with Kotlin Exposed (#57)

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     dependencies {
+        testImplementation(kotlin("test"))
         if (project !in springBootProjects) {
             testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
             testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
---
+++ comparison-with-exposed/src/test/kotlin/org/komapper/example/ApplicationTest.kt
@@ -0,0 +1,11 @@
+package org.komapper.example
+
+import kotlin.test.Test
+
+class ApplicationTest {
+
+    @Test
+    fun test() {
+        main()
+    }
+}
