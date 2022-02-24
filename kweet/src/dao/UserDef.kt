84bc27bd3565baa1be616ffc8f3546db05a564a4
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-02-25 08:15:52 +0900

    Enquote the user identifier

--- kweet/src/dao/UserDef.kt
+++ kweet/src/dao/UserDef.kt
@@ -4,8 +4,10 @@ import io.ktor.samples.kweet.model.User
 import org.komapper.annotation.KomapperColumn
 import org.komapper.annotation.KomapperEntityDef
 import org.komapper.annotation.KomapperId
+import org.komapper.annotation.KomapperTable
 
 @KomapperEntityDef(User::class)
+@KomapperTable(alwaysQuote = true)
 data class UserDef(
     @KomapperId
     val userId: Nothing,
