cce4ae91a55667fcd947bb87094926614c0e898c
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-04-09 10:49:11 +0900

    Use `-opt-in`

--- kweet/build.gradle.kts
+++ kweet/build.gradle.kts
@@ -68,6 +68,6 @@ ksp {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
     }
 }
