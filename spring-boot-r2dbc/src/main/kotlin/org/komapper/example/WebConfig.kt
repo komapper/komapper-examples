add5b080b20508f27da34579361eec81fc0335c5
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-04-29 16:16:13 +0900

    Remove sqlcommenter dependency from the WebFlux example project. (#152)
    Because sqlcommenter depends on Web MVC.

--- spring-boot-r2dbc/src/main/kotlin/org/komapper/example/WebConfig.kt
+++ spring-boot-r2dbc/src/main/kotlin/org/komapper/example/WebConfig.kt
@@ -1,26 +1,9 @@
 package org.komapper.example
 
-import com.google.cloud.sqlcommenter.interceptors.SpringSQLCommenterInterceptor
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
-/**
- * This configuration is optional.
- * See also https://github.com/google/sqlcommenter/tree/master/java/sqlcommenter-java#spring
- */
-@EnableWebMvc
+@EnableWebFlux
 @Configuration
-class WebConfig : WebMvcConfigurer {
-
-    @Bean
-    fun sqlInterceptor(): SpringSQLCommenterInterceptor {
-        return SpringSQLCommenterInterceptor()
-    }
-
-    override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(sqlInterceptor())
-    }
-}
+class WebConfig : WebFluxConfigurer
