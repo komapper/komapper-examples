8f2a6d6477deb3082a5fa067f4adc80211dd9380
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-09-10 07:04:39 +0900

    Use self mapping

--- console-jdbc/src/main/kotlin/org/komapper/example/Address.kt
+++ console-jdbc/src/main/kotlin/org/komapper/example/Address.kt
@@ -3,25 +3,18 @@ package org.komapper.example
 import org.komapper.annotation.KomapperAutoIncrement
 import org.komapper.annotation.KomapperColumn
 import org.komapper.annotation.KomapperCreatedAt
-import org.komapper.annotation.KomapperEntityDef
+import org.komapper.annotation.KomapperEntity
 import org.komapper.annotation.KomapperId
 import org.komapper.annotation.KomapperUpdatedAt
 import org.komapper.annotation.KomapperVersion
 import java.time.LocalDateTime
 
+@KomapperEntity
 data class Address(
+    @KomapperId @KomapperAutoIncrement @KomapperColumn(name = "ADDRESS_ID")
     val id: Int = 0,
     val street: String,
-    val version: Int = 0,
-    val createdAt: LocalDateTime? = null,
-    val updatedAt: LocalDateTime? = null,
-)
-
-@KomapperEntityDef(Address::class)
-data class AddressDef(
-    @KomapperId @KomapperAutoIncrement @KomapperColumn(name = "ADDRESS_ID")
-    val id: Nothing,
-    @KomapperVersion val version: Nothing,
-    @KomapperCreatedAt val createdAt: Nothing,
-    @KomapperUpdatedAt val updatedAt: Nothing,
+    @KomapperVersion val version: Int = 0,
+    @KomapperCreatedAt val createdAt: LocalDateTime? = null,
+    @KomapperUpdatedAt val updatedAt: LocalDateTime? = null,
 )
