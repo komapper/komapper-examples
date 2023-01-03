d1960c6ae59d8449f09955e9be39f52b802e66b3
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-01-03 14:29:33 +0900

    Update jvmTarget compatibility to 17 (#297)

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ subprojects {
         }
 
         withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions.jvmTarget = "11"
+            kotlinOptions.jvmTarget = "17"
         }
     }
 }
